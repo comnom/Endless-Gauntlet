49848dac877adcb680360497a4bd9d97581d9104
Frederick Goy IV <fredgoy2003@gmail.com>
2017-02-19 09:35:09 -0500

    do it right this time

--- javascripts/main.js
+++ javascripts/main.js
@@ -9,12 +9,12 @@ function apiGetLatest() {
 		if (this.readyState == 4 && this.status == 200) {
 			var data = JSON.parse(this.responseText);
 			document.getElementById("endless-gauntlet-zip").href = 
-			"https://github.com/comnom/Endless-Gauntlet/archive/" + data.tag_name + ".zip";
+			"https://github.com/comnom/Endless-Gauntlet/archive/" + data[0].tag_name + ".zip";
 			document.getElementById("endless-gauntlet-tar").href = 
-			"https://github.com/comnom/Endless-Gauntlet/archive/" + data.tag_name + ".tar.gz";
+			"https://github.com/comnom/Endless-Gauntlet/archive/" + data[0].tag_name + ".tar.gz";
 			document.getElementById("current-version").innerHTML = 
-			data.tag_name + " Released: " + data.published_at.split("T")[0];
-			document.getElementById("changelog").innerHTML = data.body;
+			data[0].tag_name + " Released: " + data[0].published_at.split("T")[0];
+			document.getElementById("changelog").innerHTML = data[0].body;
 		}
 	};
 	xhttp.open("GET", "https://api.github.com/repos/comnom/Endless-Gauntlet/releases", true);
